ab2607dd1da8e845843fa33732596e8f82e617c5
Marcia Faria <mfndnc@gmail.com>
2021-05-08 18:24:19 +0200

    prep heroku

---
+++ client/build/precache-manifest.f7bd7df2839b5b4c18ba510f9c8e17e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0670234754d3798dcb1f62cc7a3e66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1306353bce8d881992ce",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1306353bce8d881992ce",
+    "url": "/static/js/2.175014dc.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.175014dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36cc80034d36cc737c7a",
+    "url": "/static/js/main.ca8e6960.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
