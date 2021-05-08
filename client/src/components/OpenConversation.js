bedf98cac218b25a889bec88c0ce8f59ede98f71
Jonny van Essen <jonathan.vanessen@gmail.com>
2021-05-08 13:20:40 -0400

    thomas changes

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,67 +1,80 @@
-import React, { useState, useCallback } from 'react'
-import { Form, InputGroup, Button } from 'react-bootstrap'
-import { useConversations } from '../contexts/ConversationsProvider';
+import React, { useState, useCallback } from "react";
+import { Form, InputGroup, Button } from "react-bootstrap";
+import { useConversations } from "../contexts/ConversationsProvider";
 
 export default function OpenConversation() {
-  const [text, setText] = useState('')
-  const setRef = useCallback(node => {
+  const [text, setText] = useState("");
+  const setRef = useCallback((node) => {
     if (node) {
-      node.scrollIntoView({ smooth: true })
+      node.scrollIntoView({ smooth: true });
     }
-  }, [])
-  const { sendMessage, selectedConversation } = useConversations()
+  }, []);
+  const { sendMessage, selectedConversation } = useConversations();
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     sendMessage(
-      selectedConversation.recipients.map(r => r.id),
+      selectedConversation.recipients.map((r) => r.id),
       text
-    )
-    setText('')
+    );
+    setText("");
   }
 
   return (
-    <div className="d-flex flex-column flex-grow-1">
-      <div className="flex-grow-1 overflow-auto">
-        <div className="d-flex flex-column align-items-start justify-content-end px-3">
-          {selectedConversation.messages.map((message, index) => {
-            const lastMessage = selectedConversation.messages.length - 1 === index
-            return (
-              <div
-                ref={lastMessage ? setRef : null}
-                key={index}
-                className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end align-items-end' : 'align-items-start'}`}
-              >
+    <div className="mw-50">
+      <div className="d-flex flex-column flex-grow-1 h-75 mt-5 mr-5">
+        <div className="flex-grow-1 overflow-auto h-75  align-middle ">
+          <div className="d-flex flex-column align-items-start justify-content-end px-3">
+            {selectedConversation.messages.map((message, index) => {
+              const lastMessage =
+                selectedConversation.messages.length - 1 === index;
+              return (
                 <div
-                  className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
-                  {message.text}
+                  ref={lastMessage ? setRef : null}
+                  key={index}
+                  className={`my-1 d-flex flex-column ${message.fromMe
+                    ? "align-self-end align-items-end"
+                    : "align-items-start"
+                    }`}
+                >
+                  <div
+                    className={`rounded px-2 py-1 ${message.fromMe ? "bg-primary text-white" : "border"
+                      }`}
+                  >
+                    {message.text}
+                  </div>
+                  <div
+                    className={`text-muted small ${message.fromMe ? "text-right" : ""
+                      }`}
+                  >
+                    {message.fromMe ? "You" : message.senderName}
+                  </div>
                 </div>
-                <div className={`text-muted small ${message.fromMe ? 'text-right' : ''}`}>
-                  {message.fromMe ? 'You' : message.senderName}
-                </div>
-              </div>
-            )
-          })}
+              );
+            })}
+          </div>
         </div>
+
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="m-2">
+            <InputGroup>
+              <Form.Control
+                as="textarea"
+                required
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                onKeyPress={(e) => e.key === "Enter" && handleSubmit(e)}
+                style={{ height: "75px", resize: "none" }}
+              />
+
+              <InputGroup.Append>
+                <Button type="submit">Send</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form.Group>
+        </Form>
       </div>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="m-2">
-          <InputGroup>
-            <Form.Control
-              as="textarea"
-              required
-              value={text}
-              onChange={e => setText(e.target.value)}
-              onKeyPress={e => e.key === "Enter" && handleSubmit(e)}
-              style={{ height: '75px', resize: 'none' }}
-            />
-            <InputGroup.Append>
-              <Button type="submit">Send</Button>
-            </InputGroup.Append>
-          </InputGroup>
-        </Form.Group>
-      </Form>
     </div>
-  )
+  );
 }
